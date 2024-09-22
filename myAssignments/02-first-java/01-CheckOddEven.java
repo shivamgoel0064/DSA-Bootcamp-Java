690a681253fd382032501ec3807cfbc6133c8b1f
Shivam Goel <shivamgoel2024@gmail.com>
2024-09-22 22:16:33 +0530

    Update 01-CheckOddEven.java

--- myAssignments/02-first-java/01-CheckOddEven.java
+++ myAssignments/02-first-java/01-CheckOddEven.java
@@ -1,4 +1,4 @@
-// Write a program to print whether a number is even or odd, also take input from the user.
+// 1. Write a program to print whether a number is even or odd, also take input from the user.
 
 package com.learn;
 
