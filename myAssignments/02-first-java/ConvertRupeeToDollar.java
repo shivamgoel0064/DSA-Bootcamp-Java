79b79eb9fdfc3080f54591912b37efd473659d4c
Shivam Goel <shivamgoel2024@gmail.com>
2024-09-23 13:06:05 +0530

    Add files via upload

---
+++ myAssignments/02-first-java/ConvertRupeeToDollar.java
@@ -0,0 +1,17 @@
+// 6. Input currency in rupees and output in USD.
+
+
+package com.learn;
+import java.util.Scanner;
+
+public class ConvertRupeeToDollar {
+	public static void main (String[] args) {
+		float rupeesPerDollar = 83.53f;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter amount in Rupees : ");
+		float amountRupees = input.nextFloat();
+		float amountDollar = amountRupees / rupeesPerDollar;
+		System.out.println("Amount in Dollars : " + amountDollar);
+		input.close();
+	}
+}
