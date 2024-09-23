be4f6d170c44ae72022b3cae7b22ee7f320a9a14
Shivam Goel <shivamgoel2024@gmail.com>
2024-09-23 09:51:59 +0530

    Add files via upload

---
+++ myAssignments/02-first-java/CalculateSimpleInterest.java
@@ -0,0 +1,18 @@
+// 3. Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+
+package com.learn;
+import java.util.Scanner;
+
+public class CalculateSimpleInterest {
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Principal : ");
+		float p = input.nextFloat();
+		System.out.print("Time Interval (in years) : ");
+		float t = input.nextFloat();
+		System.out.print("Rate of Interest (p.a.) : ");
+		float r = input.nextFloat();
+		float si = (p * r * t) / 100;
+		System.out.println("Simple Interest -> Rs. " + si);
+	}
+}
