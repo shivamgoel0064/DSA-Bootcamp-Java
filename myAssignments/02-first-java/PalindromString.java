77f53034f28b620cda8ff1c763793516f3fd112d
Shivam Goel <shivamgoel2024@gmail.com>
2024-09-23 13:59:19 +0530

    Add files via upload

---
+++ myAssignments/02-first-java/PalindromString.java
@@ -0,0 +1,24 @@
+// 8. To find out whether the given String is Palindrome or not.
+
+package com.learn;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PalindromString {
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter any String : ");
+		String text = input.nextLine();
+		String message = text + " is a palindrome string.";
+		int length = text.length();
+		for(int i=0; i<length/2 - 1; i++) {
+			if(text.charAt(i) != text.charAt(length-i-1)) {
+				message = text + " is not a palindrome string.";
+				break;
+			}
+		}
+		System.out.println(message);
+		input.close();
+	}
+}
